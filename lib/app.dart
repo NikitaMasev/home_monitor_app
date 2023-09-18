59ccef4893575aadf1209fee6bb45f236730e5c2
Nikita Masev <n.masev@bastion-tech.ru>
2023-09-18 16:13:28 +0300

    fix changing power state rgba address(on backend side); removed theme_provider package; updated dark theme; relayout of device_rgba_address.dart - bigger tile; relayout of widget slider in led_address_config.dart - bigger tile; optimized colors for dark theme; styling status bar and bottom nav bar for dark and light theme;

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:home_monitor/di/configurator_scope.dart';
 import 'package:home_monitor/di/models/environments.dart';
 import 'package:home_monitor/internal/router/app_router.dart';
@@ -17,13 +18,40 @@ class App extends StatelessWidget {
         env: Env.dev,
         logger: true,
         initialized: initializedConfig,
-        child: MaterialApp.router(
-          scrollBehavior: ScrollNonStretching(),
-          title: 'Home Monitor',
-          routerConfig: _appRouter.config(),
-          debugShowCheckedModeBanner: false,
-          theme: ThemeFactory.createLightTheme().getTheme(),
-          darkTheme: ThemeFactory.createDarkTheme().getTheme(),
+        child: Builder(
+          builder: (final ctx) {
+            final isLightTheme = MediaQuery.of(context).platformBrightness==Brightness.light;
+
+            if (isLightTheme) {
+              SystemChrome.setSystemUIOverlayStyle(
+                const SystemUiOverlayStyle(
+                  statusBarColor: Colors.transparent,
+                  statusBarBrightness: Brightness.light,
+                  statusBarIconBrightness: Brightness.dark,
+                  systemNavigationBarColor: Color(0xFFE5EEF3),
+                  systemNavigationBarIconBrightness: Brightness.dark,
+                ),
+              );
+            } else {
+              SystemChrome.setSystemUIOverlayStyle(
+                const SystemUiOverlayStyle(
+                  statusBarColor: Colors.transparent,
+                  statusBarBrightness: Brightness.dark,
+                  statusBarIconBrightness: Brightness.light,
+                  systemNavigationBarColor: Color(0xFF1E2A30),
+                  systemNavigationBarIconBrightness: Brightness.light,
+                ),
+              );
+            }
+            return MaterialApp.router(
+              scrollBehavior: ScrollNonStretching(),
+              title: 'Home Monitor',
+              routerConfig: _appRouter.config(),
+              debugShowCheckedModeBanner: false,
+              theme: ThemeFactory.createLightTheme().getTheme(),
+              darkTheme: ThemeFactory.createDarkTheme().getTheme(),
+            );
+          },
         ),
       );
 }

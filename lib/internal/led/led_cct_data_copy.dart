c619e7941b0261fa4f30ccd03ec232d577a6076d
Nikita Masev <n.masev@bastion-tech.ru>
2023-11-03 12:30:15 +0300

    imitation anchor trailing when user selecting color temperature for led cct type device;

--- lib/internal/led/led_cct_data_copy.dart
+++ lib/internal/led/led_cct_data_copy.dart
@@ -7,9 +7,18 @@ extension LedCctDataCopy on LedCctData {
       powerOn: powerOn,
     );
 
-  LedCctData copyWithColorTemperature(final double val) => LedCctData(
+  LedCctData copyWithColorTemperature(final double val) {
+    var newColorTemp = 0;
+
+    if (val>=0.45 && val <=0.55) {
+      newColorTemp = 128;
+    } else {
+      newColorTemp = (val*255).toInt();
+    }
+    return LedCctData(
     brightness: brightness,
-    colorTemperature: (val*255).toInt(),
+    colorTemperature: newColorTemp,
     powerOn: powerOn,
   );
+  }
 }
